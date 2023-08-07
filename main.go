fe08477a870ef190ff884f131d1410bc31f56768
Rhea Ghosh <rhea@tailscale.com>
2023-08-07 11:51:01 -0500

    fixing tls & stuff

--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
     "crypto/tls"
     "fmt"
+    "log"
     "net/http"
 
     "tailscale.com/tsnet"
-    "tailscale.com/client/tailscale"
 )
 
 func main() {
-    s := &http.Server{
-           TLSConfig: &tls.Config{
-                    GetCertificate: tailscale.GetCertificate,
-           },
-          http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-             fmt.Fprintf(w, "<html><body><h1>Hello from Tailscale!</h1>\n")    
-           }),
+    s := new(tsnet.Server)
+    s.Hostname = "testhello"
+    defer s.Close()
+    ts, err := s.LocalClient() 
+    if err != nil {
+         log.Fatal(err)
     }
+
+    ln, err := s.Listen("tcp", ":443")
+    ln = tls.NewListener(ln, &tls.Config{
+              GetCertificate: ts.GetCertificate,
+           })
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    http.Serve(ln, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+             fmt.Fprintf(w, "<html><body><h1>Hello from Tailscale!</h1>\n")    
+    }))
 }
 
 
